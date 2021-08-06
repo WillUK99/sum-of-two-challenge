0a1c859b4c273fc1c7cd6bdaf1a0b2c1fdc401e3
WillUK99 <willuk1999@gmail.com>
2021-08-06 17:01:30 +0100

    Iterating over numbers and logic for finding two numbers

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello")
+
+
+
+const targetFinder = (numbers, target) => {
+    let hashStorage = {}
+    for (let number of numbers) {
+        console.log(number)
+        let numberCheckingFor = target - number
+        console.log(`Current number being iterated over is ${number} and the number we need is ${numberCheckingFor}`)
+    }
+}
+
+let target = 10
+let numbers = [1, 4, 99, 12, 10, 9]
+
+console.log(targetFinder(numbers, target))
